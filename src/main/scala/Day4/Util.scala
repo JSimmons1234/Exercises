d09bff3cecb062301e2b2aea290aab2589ef157a
Joseph Simmons <jwmsimmons@gmail.com>
2019-07-14 15:15:36 +0100

    day4/weekend

---
+++ src/main/scala/Day4/Util.scala
@@ -0,0 +1,34 @@
+package Day4
+
+object Util {
+  def printParam(param: String): Unit = {
+    println(param)
+  }
+
+  def param(param: String): String = param
+
+  def race(race: Any): Any = {
+    race
+  }
+
+  def param1(param1: String, params: Int): Unit = {
+    for (i <- 1 until param1.length if i >= (param1.length - params))
+      print(param1.charAt(i))
+    println()
+  }
+
+  def param2(str1: String, str2: String, char1: Char, char2: Char): String = {
+
+    val strFinal: String = str1 + str2
+    val array = scala.collection.mutable.ListBuffer.empty[Char]
+    array.addAll(strFinal.toList)
+    for (i <- 0 until strFinal.length) {
+      if (array(i) == char1) {
+        array(i) = char2
+        println("yes")
+      }
+    }
+    array.mkString("")
+  }
+
+}
